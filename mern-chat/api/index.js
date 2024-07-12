fa06dad0413fbc66345839ee3d3f2e14890973b9
Vidi-M <116262063+Vidi-M@users.noreply.github.com>
2024-07-12 14:04:58 +0100

    Can register and login fully using mongodb and passwords are now encrypted

--- mern-chat/api/index.js
+++ mern-chat/api/index.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 const User = require('./models/User');
 
 dotenv.config();
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MONGO_URL, {
 });
 
 const jwtSecret = process.env.JWT_SECRET;
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 const app = express();
 app.use(express.json());
@@ -44,10 +46,30 @@ app.get('/profile', (req,res) => {
     }
 });
 
+app.post('/login', async (req,res) => {
+    const {username, password} = req.body;
+    const foundUser = await User.findOne({username});
+    if (foundUser) {
+        const passOk = bcrypt.compareSync(password, foundUser.password);
+        if (passOk) {
+            jwt.sign({userId:foundUser._id, username}, jwtSecret, (err, token) => {
+                if (err) throw err;
+                res.cookie('token', token, {sameSite:'none', secure:true, httpOnly:false}).status(201).json({
+                    id: foundUser._id,
+                });
+            });
+        }
+    }
+});
+
 app.post('/register', async (req,res) => {
     const {username, password} = req.body;
-    try {    
-        const createdUser = await User.create({username, password});
+    try {
+        const hashedPassword = bcrypt.hashSync(password, bcryptSalt);
+        const createdUser = await User.create({
+            username:username, 
+            password:hashedPassword
+        });
         jwt.sign({userId:createdUser._id, username}, jwtSecret, (err, token) => {
             if (err) throw err;
             res.cookie('token', token, {sameSite:'none', secure:true, httpOnly:false}).status(201).json({
--- mern-chat/client/src/Routes.jsx
+++ mern-chat/client/src/Routes.jsx
@@ -1,4 +1,4 @@
-import Register from "./Register";
+import RegisterAndLoginForm from "./RegisterAndLoginForm.jsx";
 import { useContext } from "react";
 import { UserContext } from "./UserContext.jsx";
 
@@ -10,6 +10,6 @@ export default function Routes() {
     }
 
     return (
-        <Register />
+        <RegisterAndLoginForm />
     );
 }
