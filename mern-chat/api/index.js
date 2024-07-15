aa1bc59f6f9483d7cbb6fdb5a7b0d83ad708ae15
Vidi-M <116262063+Vidi-M@users.noreply.github.com>
2024-07-15 12:29:43 +0100

    Can detect and display who is online

--- mern-chat/api/index.js
+++ mern-chat/api/index.js
@@ -103,5 +103,9 @@ wss.on('connection', (connection, req) => {
         }
     }
 
-    console.log([...wss.clients].map(c => c.username));
+    [...wss.clients].forEach(client => {
+        client.send(JSON.stringify({
+            online: [...wss.clients].map(c => ({userId:c.userId, username:c.username}))
+        }));
+    });
 });
