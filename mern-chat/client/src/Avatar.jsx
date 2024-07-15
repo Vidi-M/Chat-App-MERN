4a957afc0e19deaf1d7e618698fe8304cdb7f449
Vidi-M <116262063+Vidi-M@users.noreply.github.com>
2024-07-15 14:25:44 +0100

    can click on specific chats and doesn't show current user in the contacts

--- mern-chat/client/src/Avatar.jsx
+++ mern-chat/client/src/Avatar.jsx
@@ -1,6 +1,6 @@
 export default function Avatar({userId, username}) {
     const colours = ['bg-red-200', 'bg-green-200', 'bg-purple-200',
-                     'bg-pink-200', 'bg-yellow-200', 'bg-orange-200', 'bg-blue-200'];
+                     'bg-pink-200', 'bg-yellow-200', 'bg-orange-200', 'bg-teal-200'];
 
     const userIdBase10 = parseInt(userId, 16);
     const colourIndex = userIdBase10 % colours.length;
