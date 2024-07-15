9158c7a105a89881879231af3dc54e030f530623
Vidi-M <116262063+Vidi-M@users.noreply.github.com>
2024-07-15 14:34:44 +0100

    some more UI improvements to contacts

--- mern-chat/client/src/Chat.jsx
+++ mern-chat/client/src/Chat.jsx
@@ -39,11 +39,16 @@ export default function Chat() {
                 {Object.keys(onlinePeopleExclOurUser).map(userId => (
                     <div key={userId}
                          onClick={() => setSelectedUserId(userId)} 
-                         className={"border-b border-gray-100 py-2 pl-4 flex items-center gap-2 cursor-pointer " + (userId === selectedUserId ? 'bg-blue-50' : '')}>
-                        <Avatar username={onlinePeople[userId]} userId={userId}/>
-                        <span className="text-gray-800">
-                            {onlinePeople[userId]}
-                        </span>
+                         className={"border-b border-gray-100 flex gap-2 cursor-pointer " + (userId === selectedUserId ? 'bg-blue-50' : '')}>
+                        {userId === selectedUserId && (
+                            <div className="w-2 bg-blue-500 h-12 rounded-r-md"></div>
+                        )}
+                        <div className="flex gap-2 py-2 pl-4 items-center">
+                            <Avatar username={onlinePeople[userId]} userId={userId}/>
+                            <span className="text-gray-800">
+                                {onlinePeople[userId]}
+                            </span>
+                        </div>
                         
                     </div>
                 ))}
