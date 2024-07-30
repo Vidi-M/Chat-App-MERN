45c3106d3656045914b7fecbc438c612afbde4fa
Vidi-M <116262063+Vidi-M@users.noreply.github.com>
2024-07-30 13:07:43 +0100

    Can now upload ans send images and files tot the chat

--- mern-chat/api/index.js
+++ mern-chat/api/index.js
@@ -6,8 +6,9 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const User = require('./models/User');
-const ws = require('ws');
 const Message = require('./models/Message');
+const ws = require('ws');
+const fs = require('fs');
 
 dotenv.config();
 
@@ -24,6 +25,7 @@ const jwtSecret = process.env.JWT_SECRET;
 const bcryptSalt = bcrypt.genSaltSync(10);
 
 const app = express();
+app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
@@ -168,19 +170,33 @@ wss.on('connection', (connection, req) => {
 
     connection.on('message', async (message) => {
         const messageData = JSON.parse(message.toString());
-        const {recipient, text} = messageData;
-        if (recipient && text) {
+        const {recipient, text, file} = messageData;
+        let filename = null;
+        if (file) {
+            const parts = file.name.split('.');
+            const ext = parts[parts.length - 1];
+            filename = Date.now() + '.' + ext;
+            const path = __dirname + '/uploads/' + filename;
+            const bufferData = Buffer.from(file.data.split(',')[1], 'base64');
+            fs.writeFile(path, bufferData, () => {
+                console.log('file saved: ' + path );
+            });
+        }
+        if (recipient && (text || file)) {
             const messageDoc = await Message.create({
                 sender: connection.userId,
                 recipient,
                 text,
+                file: file ? filename : null,
             });
+            console.log('created message');
             [...wss.clients]
                 .filter(c => c.userId === recipient)
                 .forEach(c => c.send(JSON.stringify({
                     text, 
                     sender:connection.userId,
                     recipient,
+                    file: file ? filename : null,
                     _id: messageDoc._id,
                 })));
 
--- mern-chat/client/src/Chat.jsx
+++ mern-chat/client/src/Chat.jsx
@@ -62,19 +62,36 @@ export default function Chat() {
             setMessages(prev => ([...prev, {...messageData}]));
         }
     }
-    function sendMessage(ev) { 
-        ev.preventDefault(); // so it doesn't reload the page
+    function sendMessage(ev, file = null) { 
+        if (ev) ev.preventDefault(); // so it doesn't reload the page
         ws.send(JSON.stringify({
             recipient: selectedUserId,
             text: newMessageText,
+            file,
         }));
-        setNewMessageText('');
-        setMessages(prev => ([...prev,{
-            text: newMessageText,
-            sender: id,
-            recipient: selectedUserId,
-            _id: Date.now(),
-        }]));
+        if (file) {
+            axios.get('/messages/' + selectedUserId).then(res => {
+                setMessages(res.data);
+            });
+        } else {
+            setNewMessageText('');
+            setMessages(prev => ([...prev,{
+                text: newMessageText,
+                sender: id,
+                recipient: selectedUserId,
+                _id: Date.now(),
+            }]));
+        }
+    }
+    function sendFile(ev) {
+        const reader = new FileReader();
+        reader.readAsDataURL(ev.target.files[0]);
+        reader.onload = () => {
+            sendMessage(null, {
+                name: ev.target.files[0].name,
+                data: reader.result,
+            });
+        };
     }
 
     useEffect(() => {
@@ -166,6 +183,16 @@ export default function Chat() {
                                         <div style={{ maxWidth: '66.6667%', width: 'auto' }}
                                              className={"text-left inline-block p-2 my-2 rounded-md text-sm " + (message.sender === id ? 'bg-blue-500 text-white' : 'bg-white text-gray-500')}>
                                             {message.text}
+                                            {message.file && (
+                                                <div className="">
+                                                    <a target="_blank" className="flex items-center gap-1 border-b" href={axios.defaults.baseURL + '/uploads/' + message.file}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-4">
+                                                            <path fillRule="evenodd" d="M15.621 4.379a3 3 0 0 0-4.242 0l-7 7a3 3 0 0 0 4.241 4.243h.001l.497-.5a.75.75 0 0 1 1.064 1.057l-.498.501-.002.002a4.5 4.5 0 0 1-6.364-6.364l7-7a4.5 4.5 0 0 1 6.368 6.36l-3.455 3.553A2.625 2.625 0 1 1 9.52 9.52l3.45-3.451a.75.75 0 1 1 1.061 1.06l-3.45 3.451a1.125 1.125 0 0 0 1.587 1.595l3.454-3.553a3 3 0 0 0 0-4.242Z" clipRule="evenodd" />
+                                                        </svg>
+                                                        {message.file}
+                                                    </a>
+                                                </div>
+                                            )}
                                         </div>
                                     </div>
                                 ))}
@@ -181,6 +208,12 @@ export default function Chat() {
                             onChange={ev => setNewMessageText(ev.target.value)}
                             placeholder="Type your message here" 
                             className="bg-white flex-grow border rounded-sm p-2"/>
+                        <label className="bg-blue-200 p-2 text-gray-600 cursor-pointer rounded-sm border border-blue-200">
+                            <input type="file" className="hidden" onChange={sendFile}/>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m18.375 12.739-7.693 7.693a4.5 4.5 0 0 1-6.364-6.364l10.94-10.94A3 3 0 1 1 19.5 7.372L8.552 18.32m.009-.01-.01.01m5.699-9.941-7.81 7.81a1.5 1.5 0 0 0 2.112 2.13" />
+                            </svg>
+                        </label>
                         <button type="submit" className="bg-blue-500 p-2 text-white rounded-sm">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
