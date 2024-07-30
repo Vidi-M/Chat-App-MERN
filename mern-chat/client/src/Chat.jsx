e2247764e393bafefbd8a707d782baeb175f7a6a
Vidi-M <116262063+Vidi-M@users.noreply.github.com>
2024-07-30 01:36:05 +0100

    show who's logged in

--- mern-chat/client/src/Chat.jsx
+++ mern-chat/client/src/Chat.jsx
@@ -127,10 +127,18 @@ export default function Chat() {
                             selected={userId === selectedUserId} />
                     ))}
                 </div>
-                <div className="p-2 text-center">
+                <div className="p-2 text-center flex items-center justify-center">
+                    <span className="mr-2 text-sm text-gray-600 flex items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
+                            <path fillRule="evenodd" d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" clipRule="evenodd" />
+                        </svg>
+                        {username}
+                    </span>
                     <button 
                         onClick={logout}
-                        className="text-sm bg-blue-100 py-1 px-2 text-gray-500 border rounded-sm">logout</button>
+                        className="text-sm bg-blue-100 py-1 px-2 text-gray-500 border rounded-sm">
+                            logout
+                    </button>
                 </div>
             </div>
             <div className="flex flex-col bg-blue-50 w-2/3 p-2">
